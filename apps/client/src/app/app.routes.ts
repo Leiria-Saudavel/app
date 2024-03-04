72e627e0deb5d2b7d613c50fe4f944a30dd8d5e8
Guilherme Ramalho <g.ramalho@kigroup.de>
2024-03-04 13:36:59 +0000

    Setup deployment

--- apps/client/src/app/app.routes.ts
+++ apps/client/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Route } from '@angular/router';
+import { Homepage } from './pages/home/homepage.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {path: '', component: Homepage}
+];
